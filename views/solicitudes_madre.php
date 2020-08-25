6a894d22796d160100b474bceb1b9fa1eee79323
Pedro Guzman <my.rg.developer@gmail.com>
2020-08-25 01:31:17 -0500

    Se arreglo el bug del input de busqueda modulo solicitud_madre

--- views/solicitudes_madre.php
+++ views/solicitudes_madre.php
@@ -91,7 +91,7 @@ require_once "../models/SolicitudesMadreModel.php";
         <!-- Modal -->
         <div class="modal fade" id="modalVerDetalleSolicitud" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
-                <div class="modal-content modal-lg">
+                <div style="width: 560px;" class="modal-content modal-lg">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel"></h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
